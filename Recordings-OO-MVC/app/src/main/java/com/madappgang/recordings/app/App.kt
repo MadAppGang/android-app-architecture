9fc6f014a929bf545701ffca50c02f19cc09eb2a
afedorov <afedorov@madappgang.com>
2018-06-07 23:59:24 +0300

    Add dependency container

--- Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/app/App.kt
+++ Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/app/App.kt
@@ -1,5 +1,14 @@
 package com.madappgang.recordings.app
 
+import android.app.Application
+
 /**
  * Created by Andrii Fedorov afedorov@madappgang.com on 6/7/18.
  */
+
+class App: Application() {
+    companion object {
+        val dependencyContainer by lazy { DependencyContainer() }
+    }
+
+}
--- Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/app/DependencyContainer.kt
+++ Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/app/DependencyContainer.kt
@@ -1,5 +1,18 @@
 package com.madappgang.recordings.app
 
+import com.madappgang.recordings.reckit.FileManager
+import com.madappgang.recordings.reckit.Player
+import com.madappgang.recordings.reckit.Recorder
+import com.madappgang.recordings.recnetwork.Network
+
 /**
  * Created by Andrii Fedorov afedorov@madappgang.com on 6/7/18.
  */
+class DependencyContainer {
+    val fileManager by lazy { FileManager(network) }
+    val recorder by lazy { Recorder() }
+    val player by lazy { Player(network) }
+
+//    TODO assign value
+    private lateinit var network: Network
+}
