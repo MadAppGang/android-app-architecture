29e70958d28ee598d5df2bd77cd92fb2753f737c
afedorov <afedorov@madappgang.com>
2018-06-09 02:27:06 +0300

    Make Result simpler

--- Recordings-OO-MVC/network/src/main/java/com/madappgang/recordings/network/Result.kt
+++ Recordings-OO-MVC/network/src/main/java/com/madappgang/recordings/network/Result.kt
@@ -6,43 +6,10 @@
 
 package com.madappgang.recordings.network
 
-class Result<T> private constructor(private val result: Any?) {
+sealed class Result<T> {
 
-    val isFailure: Boolean get() = result is Failure
-    val isSuccess: Boolean get() = result !is Failure
+    class Success<T>(val value: T) : Result<T>()
 
-    fun get(): T =
-            if (result is Failure) throw result.exception
-            else result as T
+    class Failure<T>(val throwable: Throwable) : Result<T>()
 
-    fun getOrNull(): T? =
-            if (result is Failure) null
-            else result as T
-
-    fun exceptionOrNull(): Throwable? =
-            (result as? Failure)?.exception
-
-    fun onTerminate(block: (Unit) -> Unit): Result<T> {
-        block(kotlin.Unit)
-        return this
-    }
-
-    fun onFailure(block: (Throwable) -> Unit): Result<T> {
-        if (isFailure) block((result as Failure).exception)
-        return this
-    }
-
-    fun onSuccess(block: (T) -> Unit): Result<T> {
-        if (isSuccess) block(result as T)
-        return this
-    }
-
-    private class Failure(@JvmField val exception: Throwable)
-
-    companion object {
-
-        fun <T> success(value: T): Result<T> = Result(value)
-
-        fun <T> failure(exception: Throwable) = Result<T>(Failure(exception))
-    }
 }
