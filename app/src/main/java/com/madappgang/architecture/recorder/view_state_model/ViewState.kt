c70643e6d9ebf7c346783d91907dcaaa009d00ee
Yaroslav Symonenko <y@madappgang.com>
2018-06-12 12:34:31 +0300

    Pre-implement view state classes

--- app/src/main/java/com/madappgang/architecture/recorder/view_state_model/ViewState.kt
+++ app/src/main/java/com/madappgang/architecture/recorder/view_state_model/ViewState.kt
@@ -1,4 +1,31 @@
 package com.madappgang.architecture.recorder.view_state_model
 
-class ViewState {
+
+data class ContentState(var folderViews: MutableList<FolderViewState>, var playerView: PlayerViewState, var recorderView: RecorderViewState? = null, var textAlert: TextAlertState? = null)
+
+data class FolderViewState(val folderUUID: String, var editing: Boolean = false, var scrollOffset: Double = 0.0) {
+    enum class Item {
+        TOGGLE_EDITING,
+        ALREADY_UPDATED_SCROLL_POSITION
+    }
+}
+
+data class RecorderViewState(var recordState: Int, val parentUUID: String) {
+    enum class Item {
+        UPDATE_RECORD_STATE
+    }
+}
+
+data class PlayerViewState(val uuid: String?, var playState: Int? = null) {
+    enum class Item {
+        UPDATE_PLAY_STATE,
+        TOGGLE_PLAY,
+        CHANGE_PLAYBACK_POSITION
+    }
+}
+
+data class TextAlertState(var text: String, val parentUUID: String, val recordingUUID: String?) {
+    enum class Item {
+        UPDATE_TEXT
+    }
 }
--- app/src/main/java/com/madappgang/architecture/recorder/view_state_model/ViewStateStore.kt
+++ app/src/main/java/com/madappgang/architecture/recorder/view_state_model/ViewStateStore.kt
@@ -1,4 +1,95 @@
 package com.madappgang.architecture.recorder.view_state_model
 
+
 class ViewStateStore {
+
+    var content = ContentState(folderViews = mutableListOf(FolderViewState(folderUUID = "")), playerView = PlayerViewState(uuid = null))
+
+    fun setPlaySelection(uuid: String?, alreadyApplied: Boolean) {
+        content.playerView = PlayerViewState(uuid = uuid)
+        //commitAction(alreadyApplied ? SplitViewState.Action.alreadyDismissedDetailView : SplitViewState.Action.changedPlaySelection)
+    }
+
+    fun pushFolder(uuid: String) {
+        content.folderViews.add(FolderViewState(folderUUID = uuid))
+        //commitAction(SplitViewState.Action.pushFolderView)
+    }
+
+    fun popToNewDepth(newDepth: Int, alreadyApplied: Boolean) {
+        val popDepth = maxOf(1, minOf(content.folderViews.size, newDepth), newDepth)
+        content.folderViews.dropLast(content.folderViews.size - popDepth)
+        //commitAction(alreadyApplied ? SplitViewState.Action.alreadyPoppedFolderView : SplitViewState.Action.popFolderView)
+    }
+
+    fun updateScrollPosition(folderUUID: String, scrollPosition: Double) {
+        val index = content.folderViews.indexOfFirst { it.folderUUID == folderUUID }
+        if (index != -1) {
+            content.folderViews[index].scrollOffset = scrollPosition
+            //commitAction(FolderViewState.Action.alreadyUpdatedScrollPosition(folderUUID))
+        }
+    }
+
+    fun toggleEditing(folderUUID: String) {
+        val index = content.folderViews.indexOfFirst { it.folderUUID == folderUUID }
+        if (index != -1) {
+            content.folderViews[index].editing = !content.folderViews[index].editing
+            //commitAction(FolderViewState.Action.toggleEditing(folderUUID))
+        }
+    }
+
+    fun showCreateFolder(parentUUID: String) {
+        content.textAlert = TextAlertState(text = "", parentUUID = parentUUID, recordingUUID = null)
+        //commitAction(SplitViewState.Action.showTextAlert)
+    }
+
+    fun showRecorder(parentUUID: String) {
+        content.recorderView = RecorderViewState(recordState = 0, parentUUID = parentUUID)
+        //commitAction(SplitViewState.Action.showRecordView)
+    }
+
+    fun dismissRecording() {
+        content.recorderView = null
+        //commitAction(SplitViewState.Action.dismissRecordView)
+    }
+
+    fun showSaveRecording(uuid: String, parentUUID: String) {
+        content.textAlert = TextAlertState(text = "", parentUUID = parentUUID, recordingUUID = uuid)
+        //commitAction(SplitViewState.Action.showTextAlert)
+    }
+
+    fun dismissTextAlert() {
+        content.textAlert = null
+        //commitAction(SplitViewState.Action.dismissTextAlert)
+    }
+
+    fun updateAlertText(text: String) {
+        content.textAlert?.text = text
+        //commitAction(TextAlertState.Action.updateText)
+    }
+
+    fun updatePlayState(playState: Int) {
+        content.playerView.playState = playState
+        //commitAction(PlayViewState.Action.updatePlayState, sideEffect: true)
+    }
+
+    fun updateRecordState(recordState: Int) {
+        content.recorderView?.recordState = recordState
+        //commitAction(RecordViewState.Action.updateRecordState, sideEffect: true)
+    }
+
+    fun togglePlay() {
+        if (content.playerView.playState != null) {
+            content.playerView.playState = 1   //playState.isPlaying = !playState.isPlaying
+            //commitAction(PlayViewState.Action.togglePlay)
+        }
+    }
+
+    fun changePlaybackPosition(position: Double) {
+        if (content.playerView.playState != null) {
+            val previousState = content.playerView.playState
+            //content.playerView.playState = PlayerState(isPlaying = previousState.isPlaying, progress = position, duration = previousState.duration)
+            //commitAction(PlayViewState.Action.changePlaybackPosition)
+        }
+    }
+
 }
