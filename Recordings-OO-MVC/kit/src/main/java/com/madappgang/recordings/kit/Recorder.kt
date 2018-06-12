70448c55c78f780d462041d324353cb529ca986a
afedorov <afedorov@madappgang.com>
2018-06-13 00:37:52 +0300

    Implement Recorder

--- Recordings-OO-MVC/kit/src/main/java/com/madappgang/recordings/kit/Recorder.kt
+++ Recordings-OO-MVC/kit/src/main/java/com/madappgang/recordings/kit/Recorder.kt
@@ -6,20 +6,63 @@
 
 package com.madappgang.recordings.kit
 
+import com.googlecode.mp4parser.authoring.Movie
 import com.madappgang.recordings.core.Track
 import java.io.File
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator
+import java.io.RandomAccessFile
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class Recorder {
 
+    private val sourceFiles = mutableListOf<String>()
+
     fun start() {
-        TODO("not implemented")
+        removeParts(sourceFiles)
+        sourceFiles.clear()
     }
 
     fun addPart(file: File) {
-        TODO("not implemented")
+        sourceFiles.add(file.absolutePath)
     }
 
     fun stop(): Track {
-        TODO("not implemented")
+        val outputFileName = getOutputFileName()
+        mergeAudioFiles(sourceFiles, outputFileName)
+        removeParts(sourceFiles)
+        return Track(url = outputFileName)
     }
+
+    private fun getOutputFileName(): String {
+        val folder = File(sourceFiles.first()).absolutePath
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmssSSS")
+        return "${folder}recording_${dateFormat.format(Date())}.m4a"
+    }
+
+    private fun mergeAudioFiles(sourceFiles: List<String>, outputFileName: String) {
+        val listTracks = sourceFiles.map { MovieCreator.build(it) }
+                .flatMap { it.tracks }
+                .filter { it.handler == "soun" }
+                .toList()
+
+        val outputMovie = Movie()
+        if (!listTracks.isEmpty()) {
+            outputMovie.addTrack(AppendTrack(*listTracks.toTypedArray()))
+        }
+        val container = DefaultMp4Builder().build(outputMovie)
+        val fileChannel = RandomAccessFile(String.format(outputFileName), "rw").channel
+        container.writeContainer(fileChannel)
+        fileChannel.close()
+    }
+
+    private fun removeParts(sourceFiles: List<String>) = sourceFiles
+            .map { File(it) }
+            .filter { it.exists() }
+            .forEach {
+                it.delete()
+            }
 }
