e7aada5606719a339106703de4a91497758dd519
afedorov <afedorov@madappgang.com>
2018-06-14 15:06:09 +0300

    Add separate view for recorder

--- Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/activities/RecorderActivity.kt
+++ Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/activities/RecorderActivity.kt
@@ -31,6 +31,7 @@ import com.madappgang.recordings.extensions.makeVisible
 import com.madappgang.recordings.extensions.showError
 import com.madappgang.recordings.kit.Recorder
 import com.madappgang.recordings.kit.validName
+import com.madappgang.recordings.layoutmanagers.RecorderView
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
 import java.io.File
@@ -41,35 +42,45 @@ class RecorderActivity :
     EditableDialogFragment.CompletionHandler,
     EditableDialogFragment.FieldValidationHandler {
 
-    private val permissionsRequestRecordAudio = 3526
-    private val saveTrackRequestId = "saveTrackRequestId"
-
-    private val folder by lazy { intent.getParcelableExtra(FOLDER_KEY) as Folder }
-
-    private val uiContext by lazy { UI }
-    private val bgContext by lazy { CommonPool }
-    private val fileManager by lazy { App.dependencyContainer.fileManager }
-    private val recorder by lazy { App.dependencyContainer.recorder }
-
-    private val time by lazy { findViewById<TextView>(R.id.time) }
-    private val startRecording by lazy { findViewById<Button>(R.id.startRecording) }
-    private val pauseResumeRecording by lazy { findViewById<Button>(R.id.pauseResumeRecording) }
-    private val stopRecording by lazy { findViewById<Button>(R.id.stopRecording) }
+    private val recorderView by lazy { findViewById<RecorderView>(R.id.recorderView) }
     private val progressBar by lazy { findViewById<ProgressBar>(R.id.progressBar) }
+
+    private val recorder by lazy { App.dependencyContainer.recorder }
+    private val fileManager by lazy { App.dependencyContainer.fileManager }
 
     private lateinit var track: Track
 
     private var updateTimeJob: Job? = null
     private var saveTrackJob: Job? = null
+    private val uiContext by lazy { UI }
+    private val bgContext by lazy { CommonPool }
+
+    private val folder by lazy { intent.getParcelableExtra(FOLDER_KEY) as Folder }
+
+    private val permissionsRequestRecordAudio = 3526
+    private val saveTrackRequestId = "saveTrackRequestId"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recorder)
 
+        recorderView.onStartRecording = {
+            if (isMicrophonePermissionGranted()) {
+                startRecording()
+            } else {
+                requestMicrophonePermission()
+            }
+        }
+        recorderView.onStopRecording = { stopRecording() }
+        recorderView.onPauseResumeRecording = { pauseResumeRecording() }
+
         recorder.reset()
-        recorder.status.observe(this, Observer<Recorder.Status> { updateButton() })
-        updateTime()
-        initButton()
+        recorder.status.observe(this, Observer<Recorder.Status> {
+            updateUi()
+            updateUiJob()
+        })
+
+        updateUi()
     }
 
     override fun onRequestPermissionsResult(
@@ -117,46 +128,21 @@ class RecorderActivity :
         removeTrack()
     }
 
-    private fun initButton() {
-        initStartRecordingButton()
-        initPauseResumeRecordingButton()
-        initStopRecordingButton()
-    }
-
-    private fun initStartRecordingButton() {
-        startRecording.setOnClickListener {
-            if (isMicrophonePermissionGranted()) {
-                startRecording()
-            } else {
-                requestMicrophonePermission()
-            }
-        }
-    }
-
-    private fun initPauseResumeRecordingButton() {
-        pauseResumeRecording.setOnClickListener {
-            if (recorder.status.value == Recorder.Status.STARTED) {
-                recorder.pause()
-                updateTimeJob?.cancel()
-            } else if (recorder.status.value == Recorder.Status.PAUSED) {
-                updateTimeJob = createUpdateTimeJob()
-                recorder.resume()
-            }
-        }
-    }
-
-    private fun initStopRecordingButton() {
-        stopRecording.setOnClickListener {
-            track = recorder.stop()
-            updateTimeJob?.cancel()
-            showSaveTrackDialog()
+    private fun pauseResumeRecording() {
+        if (recorder.status.value == Recorder.Status.STARTED) {
+            recorder.pause()
+        } else if (recorder.status.value == Recorder.Status.PAUSED) {
+            recorder.resume()
         }
     }
 
     private fun startRecording() {
-        updateTimeJob = createUpdateTimeJob()
         recorder.start()
-        updateButton()
+    }
+
+    private fun stopRecording() {
+        track = recorder.stop()
+        showSaveTrackDialog()
     }
 
     private fun isMicrophonePermissionGranted() =
@@ -190,52 +176,23 @@ class RecorderActivity :
         }
     }
 
-    private fun updateButton() {
-        when (recorder.status.value) {
-            Recorder.Status.NOT_STARTED -> applyNotStartedStateForButton()
-            Recorder.Status.STARTED -> applyStartedStateForButton()
-            Recorder.Status.PAUSED -> applyPausedStateForButton()
-            Recorder.Status.COMPLETED -> applyCompletedStateForButton()
+    private fun updateUiJob() {
+        updateTimeJob?.cancel()
+        if (recorder.status.value == Recorder.Status.STARTED) {
+            updateTimeJob = createUpdateTimeJob()
         }
     }
 
     private fun createUpdateTimeJob() = launch(uiContext) {
         while (true) {
-            updateTime()
             delay(1, TimeUnit.SECONDS)
+            updateUi()
         }
     }
 
-    private fun updateTime() {
-        time.text = time.formatMilliseconds(recorder.getRecordingTime())
-    }
-
-    private fun applyNotStartedStateForButton() {
-        startRecording.isEnabled = true
-        pauseResumeRecording.isEnabled = false
-        pauseResumeRecording.text = getString(R.string.RecorderActivity_pause)
-        stopRecording.isEnabled = false
-    }
-
-    private fun applyStartedStateForButton() {
-        startRecording.isEnabled = false
-        pauseResumeRecording.isEnabled = true
-        pauseResumeRecording.text = getString(R.string.RecorderActivity_pause)
-        stopRecording.isEnabled = true
-    }
-
-    private fun applyPausedStateForButton() {
-        startRecording.isEnabled = false
-        pauseResumeRecording.isEnabled = true
-        pauseResumeRecording.text = getString(R.string.RecorderActivity_resume)
-        stopRecording.isEnabled = true
-    }
-
-    private fun applyCompletedStateForButton() {
-        startRecording.isEnabled = false
-        pauseResumeRecording.isEnabled = false
-        pauseResumeRecording.text = getString(R.string.RecorderActivity_pause)
-        stopRecording.isEnabled = false
+    private fun updateUi() {
+        recorder.status.value?.let { recorderView.setStatus(it) }
+        recorderView.setTime(recorder.getRecordingTime())
     }
 
     private fun showSaveTrackDialog(defaultName: String = "") {
---
+++ Recordings-OO-MVC/app/src/main/java/com/madappgang/recordings/views/RecorderView.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 MadAppGang.
+ *
+ * Created by Andrii Fedorov afedorov@madappgang.com on 6/13/18.
+ */
+
+package com.madappgang.recordings.layoutmanagers
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.TextView
+import com.madappgang.recordings.R
+import com.madappgang.recordings.extensions.formatMilliseconds
+import com.madappgang.recordings.kit.Recorder
+
+
+class RecorderView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    var onStartRecording: () -> Unit = {}
+    var onStopRecording: () -> Unit = {}
+    var onPauseResumeRecording: () -> Unit = {}
+
+    private var status = Recorder.Status.NOT_STARTED
+
+    private val time by lazy { findViewById<TextView>(R.id.time) }
+    private val startRecording by lazy { findViewById<Button>(R.id.startRecording) }
+    private val pauseResumeRecording by lazy { findViewById<Button>(R.id.pauseResumeRecording) }
+    private val stopRecording by lazy { findViewById<Button>(R.id.stopRecording) }
+
+
+    init {
+        inflate(getContext(), R.layout.view_recorder, this)
+
+        stopRecording.setOnClickListener { onStopRecording.invoke() }
+        startRecording.setOnClickListener { onStartRecording.invoke() }
+        pauseResumeRecording.setOnClickListener { onPauseResumeRecording.invoke() }
+    }
+
+    fun setStatus(status: Recorder.Status) {
+        this.status = status
+        updateButton()
+    }
+
+    fun setTime(millisecond: Int) {
+        time.text = time.formatMilliseconds(millisecond)
+    }
+
+    private fun updateButton() {
+        when (status) {
+            Recorder.Status.NOT_STARTED -> applyNotStartedStateForButton()
+            Recorder.Status.STARTED -> applyStartedStateForButton()
+            Recorder.Status.PAUSED -> applyPausedStateForButton()
+            Recorder.Status.COMPLETED -> applyCompletedStateForButton()
+        }
+    }
+
+    private fun applyNotStartedStateForButton() {
+        startRecording.isEnabled = true
+        pauseResumeRecording.isEnabled = false
+        pauseResumeRecording.text = context.getString(R.string.RecorderActivity_pause)
+        stopRecording.isEnabled = false
+    }
+
+    private fun applyStartedStateForButton() {
+        startRecording.isEnabled = false
+        pauseResumeRecording.isEnabled = true
+        pauseResumeRecording.text = context.getString(R.string.RecorderActivity_pause)
+        stopRecording.isEnabled = true
+    }
+
+    private fun applyPausedStateForButton() {
+        startRecording.isEnabled = false
+        pauseResumeRecording.isEnabled = true
+        pauseResumeRecording.text = context.getString(R.string.RecorderActivity_resume)
+        stopRecording.isEnabled = true
+    }
+
+    private fun applyCompletedStateForButton() {
+        startRecording.isEnabled = false
+        pauseResumeRecording.isEnabled = false
+        pauseResumeRecording.text = context.getString(R.string.RecorderActivity_pause)
+        stopRecording.isEnabled = false
+    }
+}
